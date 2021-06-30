7afe304bb03b3ef72956f3db25368364ad159550
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 17:05:42 +0300

    Rokas

--- data/tweets.tsx
+++ data/tweets.tsx
@@ -6,7 +6,13 @@ const tweets = [
             username: 'rookasrudzianskas',
             name: "Rokas Rudzianskas",
             image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
-        }
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: "something in here",
+        image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
+        numberOfComments: 123,
+        numberOfRetweets: 11,
+        numberOfLikes: 10,
     }
 ];
 
