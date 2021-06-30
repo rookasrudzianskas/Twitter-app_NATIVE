b460b015a8be2a7e8ed3a6bdfff7334b9f678fef
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 16:50:47 +0300

    Rokas

--- components/Tweet/index.tsx
+++ components/Tweet/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text} from "react-native";
-
+import MainContainer from "./MainContainer";
+import LeftContainer from "./LeftContainer";
 export type TweetProps = {
 
 }
@@ -8,10 +9,11 @@ export type TweetProps = {
 const Tweet = (props: TweetProps) => {
     <View>
     {/*    Left  Container */}
+        <LeftContainer />
 
 
     {/*    Main  Container */}
-
+        <MainContainer />
     </View>
 }
 
