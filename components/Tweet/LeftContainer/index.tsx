acf557ab5500de38581da10cdd6e0385688fc2b3
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 16:46:35 +0300

    Rokas

---
+++ components/Tweet/LeftContainer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+export type LeftContainerProps = {
+
+}
+
+const LeftContainer = (props: LeftContainerProps) => {
+    <View>
+    {/*    Profile picture */}
+
+    </View>
+}
+
+export default LeftContainer;
