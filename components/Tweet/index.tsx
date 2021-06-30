0cce007b830d96b08efe8f3abe85e00d16661acd
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 16:44:43 +0300

    Rokas

---
+++ components/Tweet/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+export type TweetProps = {
+
+}
+
+const Tweet = (props: TweetProps) => {
+    <View>
+    {/*    Left  bar */}
+
+    </View>
+}
+
+export default Tweet;
