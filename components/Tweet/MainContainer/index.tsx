f105ccfabfc83c1b313083fac7a4959c171244af
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 16:47:09 +0300

    Rokas

---
+++ components/Tweet/MainContainer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+export type MainContainerProps = {
+
+}
+
+const MainContainer = (props: MainContainerProps) => {
+    <View>
+    {/*    Profile picture */}
+
+    </View>
+}
+
+export default MainContainer;
