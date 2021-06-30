b1adef35e426a8332160a0d3df665b908d6a5b7b
Rokas <rookas.rudzenskas@gmail.com>
2021-06-30 17:01:08 +0300

    Rokas

--- data/tweets.tsx
+++ data/tweets.tsx
@@ -1,7 +1,12 @@
 const tweets = [
     {
-        id: '1',
-
+        id: 't1',
+        user: {
+            id: 'u1',
+            username: 'rookasrudzianskas',
+            name: "Rokas Rudzianskas",
+            image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
+        }
     }
 ];
 
