f74a44474d2c86803b1567eaf5a069f76f9e676d
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:45:34 +0300

    Rokas

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -6,12 +6,14 @@ import { Text, View } from '../components/Themed';
 import tweets from "../data/tweets";
 import Feed from "../components/Feed";
 import NewTweetButton from "../components/NewTweetButton";
+import {EvilIcons, AntDesign} from "@expo/vector-icons";
+import Colors from "../constants/Colors";
 
 export default function NewTweetScreen() {
   return (
     <View>
       <View>
-
+            <EvilIcons name="close" size={30} color={Colors.light.tint} />
       </View>
     </View>
   );
