443c1ea53939bfbe508306610fa64e64358fc891
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:26:16 +0300

    Rokas

--- components/NewTweetButton/index.tsx
+++ components/NewTweetButton/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import {View, Text, TouchableOpacity} from "react-native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const NewTweetButton = () => {
     return (
         <TouchableOpacity>
-
+            <MaterialCommunityIcons name="feather" size={24} color="black" />
         </TouchableOpacity>
     )
 }
