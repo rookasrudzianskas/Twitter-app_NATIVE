f288a2bcabe7f3f23602c3ec5065f6528f8b149f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 06:21:50 +0300

    Rokas

--- components/Tweet/MainContainer/index.tsx
+++ components/Tweet/MainContainer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, Image} from "react-native";
 import {TweetType} from "../../../types";
 import styles from "./styles";
+import {Ionicons} from "@expo/vector-icons";
 
 export type MainContainerProps = {
     tweet: TweetType
@@ -14,6 +15,7 @@ const MainContainer = ({tweet}: MainContainerProps) => {
                 <Text style={styles.name}>{tweet.user.name}</Text>
                 <Text style={styles.username}>@{tweet.user.username}</Text>
                 <Text style={styles.createdAt}>15s.</Text>
+                <Ionicons name="chevron-down" />
             </View>
             <View>
                 <Text>{tweet.content}</Text>
