51454d59f59431cc74f0173db244a188d1520110
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:14:37 +0300

    Rokas

--- data/tweets.tsx
+++ data/tweets.tsx
@@ -13,7 +13,39 @@ const tweets = [
         numberOfComments: 123,
         numberOfRetweets: 11,
         numberOfLikes: 10,
-    }
+    },
+
+    {
+        id: 't2',
+        user: {
+            id: 'u2',
+            username: 'sundar',
+            name: "Sundar Pichai",
+            image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand.",
+        image: '',
+        numberOfComments: 123,
+        numberOfRetweets: 11,
+        numberOfLikes: 10,
+    },
+
+    {
+        id: 't3',
+        user: {
+            id: 'u3',
+            username: 'timcook',
+            name: "Tim Cook",
+            image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
+        },
+        createdAt: '2020-08-27T12:00:00.000Z',
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown.",
+        image: 'https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg',
+        numberOfComments: 123,
+        numberOfRetweets: 11,
+        numberOfLikes: 10,
+    },
 ];
 
 export default tweets;
