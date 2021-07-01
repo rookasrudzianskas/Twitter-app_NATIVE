ee8200245e59ce3040785fcb310f9c42393ad2b9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:25:07 +0300

    Rokas

---
+++ components/NewTweetButton/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text, TouchableOpacity} from "react-native";
+
+const NewTweetButton = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default NewTweetButton;
