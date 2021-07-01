c3dc2d19102f4fb39e24998217395ee22379c6c9
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:31:29 +0300

    Rokas

--- components/NewTweetButton/style.ts
+++ components/NewTweetButton/style.ts
@@ -2,7 +2,9 @@ import {StyleSheet} from "react-native";
 import Colors from "../../constants/Colors";
 
 const styles = StyleSheet.create({
-
+    button: {
+        backgroundColor: Colors.light.tint,
+    }
 })
 
 export default styles;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -5,11 +5,13 @@ import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import tweets from "../data/tweets";
 import Feed from "../components/Feed";
+import NewTweetButton from "../components/NewTweetButton";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Feed/>
+      <NewTweetButton />
     </View>
   );
 }
