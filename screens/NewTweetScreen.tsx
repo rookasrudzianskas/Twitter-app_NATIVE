47b6505e41abe533a30335e6df4c3cff72129625
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:36:51 +0300

    Rokas

---
+++ screens/NewTweetScreen.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import Tweet from "../components/Tweet";
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import tweets from "../data/tweets";
+import Feed from "../components/Feed";
+import NewTweetButton from "../components/NewTweetButton";
+
+export default function NewTweetScreen() {
+  return (
+    <View style={styles.container}>
+      <Feed/>
+      <NewTweetButton />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
