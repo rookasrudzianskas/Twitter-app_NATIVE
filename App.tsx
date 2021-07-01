ded59c045388e97681cf9dd523d15f1998b19b6e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 09:11:04 +0300

    Rokas

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
+// @ts-ignore
 import { withAuthenticator } from "aws-amplify-react-native";
 
 Amplify.configure(config)
