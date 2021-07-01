af7f480d1652baca539b9dbe2072cb446883f8a7
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 07:51:19 +0300

    Rokas

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import Tweet from "../components/Tweet";
 import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View, TouchableOpacity } from 'react-native';
 import tweets from "../data/tweets";
 import Feed from "../components/Feed";
 import NewTweetButton from "../components/NewTweetButton";
@@ -10,12 +10,39 @@ import {EvilIcons, AntDesign} from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 
 export default function NewTweetScreen() {
+
+    const onPostTweet = () => {
+        console.log("on post tweet");
+    }
   return (
-    <View>
+    <View style={styles.container}>
       <View>
-            <EvilIcons name="close" size={30} color={Colors.light.tint} />
+            <AntDesign name="close" size={30} color={Colors.light.tint} />
+          <TouchableOpacity style={styles.button} onPress={onPostTweet}>
+            <Text style={styles.buttonText}>Tweet</Text>
+          </TouchableOpacity>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: Colors.light.tint,
+        borderRadius: 30,
+
+    },
+    buttonText: {
+      paddingHorizontal: 20,
+      paddingVertical: 10,
+      color: "white",
+      fontWeight: "bold",
+    },
+
+})
 
