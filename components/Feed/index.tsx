dde4f146c9eec8346132c960d3211014eb107999
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 11:02:24 +0300

    Rokas

--- components/Feed/index.tsx
+++ components/Feed/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, FlatList} from "react-native";
-import tweets from "../../data/tweets";
+// import tweets from "../../data/tweets";
+import {API, graphqlOperation} from "aws-amplify";
+import {listTweets} from "../../graphql/queries";
 import Tweet from "../Tweet";
 
+
 const Feed = () => {
+
+    const [tweets, setTweets] = useState([]);
+
+    useEffect(() => {
+        // get all the data, then component mounts
+
+        const fetchTweets = async () => {
+            // get the tweets from the backend and set them to the state
+
+        }
+    }, []);
+
+
     return (
         <View style={{width:"100%"}}>
             <FlatList  showsVerticalScrollIndicator={false}
